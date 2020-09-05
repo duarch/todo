7bb8265e73c6607499d68a47b92cff41f2c4c68c
duarch <@users.noreply.github.com>
2020-09-05 18:35:57 -0300

    add a lot of stuff

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 
+$("#plus").click(function () {
+    $("#new").fadeToggle("linear");
+});
+
+
+
+$("#newtask").keydown(function (e) {
+    if (e.which == 13) {
+        var inputVal = $(this).val();
+        $("#list").append("<li class='list-group-item todo'>"+inputVal+"</li>")
+    }
+})
+
+
+$( ".todo" ).hover(
+    function() {
+      $( this ).prepend( $( "<span class='trash'><i class='fas fa-trash'></i></span>" ) );
+    }, function() {
+      $( this ).find( "span" ).last().remove();
+    }
+    
+  );
+// function(){
+//     $(this).toggle("slide","right")
+//     }
