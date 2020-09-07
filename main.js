64d34360ebced2648328a26438df9aace26a9806
duarch <@users.noreply.github.com>
2020-09-07 14:00:44 -0300

    ajustments

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
 
 $("#plus").click(function () {
-    $("#new").fadeToggle("linear");
+  $("#new").fadeToggle("linear");
 });
 
 
 
 $("#newtask").keydown(function (e) {
-    if (e.which == 13) {
-        var inputVal = $(this).val();
-        $("#list").append("<li class='list-group-item todo'>"+inputVal+"</li>")
-    }
+  if (e.which == 13) {
+    var inputVal = $(this).val();
+    $("#list").append("<li class='list-group-item todo'>" + inputVal + "</li>")
+  }
 })
 
 
-$( ".todo" ).hover(
-    function() {
-      $( this ).prepend( $( "<span class='trash'><i class='fas fa-trash'></i></span>" ) );
-    }, function() {
-      $( this ).find( "span" ).last().remove();
-    }
+$(".todo").hover(
+  function () {
+    $(this).addClass("pl-0")
+    $(this).prepend($("<span class='trash'><i class='fas fa-trash'></i></span>"));
+  }, function () {
+    $(this).find("span").last().remove();
+    $(this).removeClass("pl-0")
     
-  );
+  }
+
+);
+
+$("li").click(function () {
+  $(this).toggleClass("completed")
+})
+
+
+
+
+
+
+
+
 // function(){
 //     $(this).toggle("slide","right")
 //     }
